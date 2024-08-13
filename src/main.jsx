9510775e3e447a01ab73e86fef8df226ae30b2f0
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-08-12 18:09:19 -0600

    inicializando los routers

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+// react router dom
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Menu</h1>,
+    children: [
+      {
+        path: '/login',
+        element: <h1>Login</h1>
+      },
+      {
+        path: '/register',
+        element: <h1>Register</h1>
+      },
+      {
+        path: '/game',
+        element: <h1>Game</h1>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )

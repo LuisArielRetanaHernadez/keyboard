b65b46b19d368e38bf92994bf7f65d9af7192c5b
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-08-14 21:17:33 -0600

    creando el componente logo con su estructura inicial

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+  return (
+    <div className="logo">
+      <h2 className="logo__text">Keyboard <span className="logo__macro">Mabi</span></h2>
+    </div>
+  )
+}
+
+export default Logo
